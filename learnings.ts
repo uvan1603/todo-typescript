e8ee3731bd6c89b4bcc9329a9ef7a4140792ee68
uvan1603 <uvannarayanan1603@gmail.com>
2025-10-01 12:34:47 +0530

    First commit for typescript

---
+++ learnings.ts
@@ -0,0 +1,8 @@
+let myString: string = "Hello , my name is Uvan";
+console.log(myString);
+
+function addTwoumbers(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addTwoumbers(35, 2));
