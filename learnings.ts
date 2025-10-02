ee55dafb203cbc1a288f757b54e80ed1afa26411
uvan1603 <uvannarayanan1603@gmail.com>
2025-10-02 12:43:10 +0530

    initial commit

--- learnings.ts
+++
@@ -1,8 +0,0 @@
-let myString: string = "Hello , my name is Uvan";
-console.log(myString);
-
-function addTwoumbers(a: number, b: number): number {
-  return a + b;
-}
-
-console.log(addTwoumbers(35, 2));
